aca75efedbeff75c48c9c39190280a7eb89722d7
student <student@w431-imac01.ccsd.d5>
2017-04-07 07:48:08 -0600

    enemy edit

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,24 @@
 
-public abstract class Enemy {
+public class Enemy {
+	private int health;
+	private  int damage;
+	public Enemy(int h, int d){
+		health = h;
+		damage = d;
+	}
+	public int getHealth(){
+		return health;
+	}
+	public void reduceHealth(int num){
+		health -= num;
+	}
+	public int getDamage(){
+		return damage;
+	}
+	public boolean isDead(){
+		if(health < 0)
+			return true;
+		return false;
+	}
 
 }
