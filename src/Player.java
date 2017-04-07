1db50d64849bd4d30ccfdf048cac371ce45c7768
student <student@w431-imac01.ccsd.d5>
2017-04-07 07:59:47 -0600

    player edit

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,20 @@
 
 public class Player {
-
+	private int health;
+	private int damage;
+	//private ArrayList <Item> items;
+	
+	public Player(){
+		health = 100;
+		damage = 20;
+	}
+	public int getHealth(){
+		return health;
+	}
+	public int getDamage(){
+		return damage;
+	}
+	public void reduceHealth(int num){
+		health -= num;
+	}
 }
