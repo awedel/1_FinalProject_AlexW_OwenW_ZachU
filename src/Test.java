095a2919172b40809681fe4fc7b73232d7f95bb4
student <student@w431-imac01.ccsd.d5>
2017-04-12 17:57:02 -0600

    start of "mockgame"

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,17 @@
-
+import java.util.Scanner;
 public class Test {
-	
+	public void mockGame(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Game Start");
+		Player guy = new Player();
+		while(guy.getHealth() < 0){
+			System.out.println("Enter 'attack' to attack the Enemy");
+			String input = sc.nextLine();
+			if(input == "attack"){
+				
+			}
+		}
+	}
 	public static void main(String[] args) {
 		
 
