0d5bb4e19b76017c914c3bff81d4f60aaf1791ca
student <student@w431-imac01.ccsd.d5>
2017-04-13 17:04:38 -0600

    gg

--- src/Item.java
+++ src/Item.java
@@ -10,15 +10,10 @@ public abstract class Item {
 	public String toString(){
 		return this.getName();
 	}
-	
-<<<<<<< HEAD
 	public void addToInventory(Player player){
 		player.getInventory().add(this);
 	}
 	public abstract void effect(Player player);
 	public abstract void effect(Enemy enemy);
 
-=======
-xD
->>>>>>> branch 'master' of https://github.com/awedel/1_FinalProject_AlexW_OwenW_ZachU.git
 }
