e2362e254a8448d5eb468a0491fac2a9a978780d
student <student@w431-imac01.ccsd.d5>
2017-04-17 16:17:36 -0600

    first changes to weapon

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,4 +1,14 @@
 
-public class Weapon {
-// changes
+public abstract class Weapon {
+	private int damage;
+	private String name;
+	
+	public Weapon(String n, int d){
+		name = n;
+		damage = d;
+	}
+	public String getName(){
+		return name;
+	}
+	
 }
