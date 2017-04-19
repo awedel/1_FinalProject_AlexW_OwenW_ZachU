0829db67b47cdaccac17b1e34765ede7e6d300a0
student <student@w431-imac05.ccsd.d5>
2017-04-19 15:21:16 -0600

    DisplayChanges

--- src/Display.java
+++ src/Display.java
@@ -1,9 +1,20 @@
 
+
+
 public class Display {
 
 	public static void main(String[] args) {
+		StdDraw.filledRectangle(.5, .5, .5, .5);
 		
-
+		StdDraw.picture(.5, .5, "New Piskel.png");
+		try {
+			Thread.sleep(30000);
+			StdDraw.clear();
+			StdDraw.rectangle(.25, .25, .1, .1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
