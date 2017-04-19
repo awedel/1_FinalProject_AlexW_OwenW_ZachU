c5432737877cc64cadc99d6a3f300e98c8e5ea96
student <student@w431-imac05.ccsd.d5>
2017-04-19 15:36:10 -0600

    Display and stdlib

--- src/Display.java
+++ src/Display.java
@@ -1,20 +1,11 @@
 
-
-
 public class Display {
 
 	public static void main(String[] args) {
-		StdDraw.filledRectangle(.5, .5, .5, .5);
-		
+		StdDraw.filledSquare(.5, .5, .5);
 		StdDraw.picture(.5, .5, "New Piskel.png");
-		try {
-			Thread.sleep(30000);
-			StdDraw.clear();
-			StdDraw.rectangle(.25, .25, .1, .1);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		StdDraw.pause(5000);
+		StdDraw.picture(.3, .3, "PlayerNoItemPNG.png");
 	}
 
 }
