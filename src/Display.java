b64bf99bd2d8babd17b79e70cc860727856f8c76
student <student@w431-imac05.ccsd.d5>
2017-04-19 16:43:49 -0600

    DisplayChanges

--- src/Display.java
+++ src/Display.java
@@ -1,7 +1,13 @@
+import java.awt.* ;
+import java.awt.event.* ;
+import javax.swing.*;
+
 
 public class Display {
 	
-	
+	private JFrame mainFrame ;
+	private JLabel headerLabel ;
+	private JLabel statusLabel ;
 	
 	
 	public static void main(String[] args) {
