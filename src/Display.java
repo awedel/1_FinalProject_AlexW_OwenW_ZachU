f9b8d065d521b7977d3c124f6c61a289653ec0a4
student <student@w431-imac05.ccsd.d5>
2017-04-19 16:43:36 -0600

    Display Changes

--- src/Display.java
+++ src/Display.java
@@ -1,14 +1,13 @@
 
 public class Display {
-
+	
+	
+	
+	
 	public static void main(String[] args) {
-		StdDraw.filledSquare(.5, .5, .5);
-		StdDraw.picture(.5, .5, "New Piskel.png");
-		StdDraw.pause(5000);
-		StdDraw.picture(.3, .3, "PlayerNoItemPNG.png");
-		StdDraw.picture(.7, .7, "gif.gif");
-		StdDraw.pause(800);
-		StdDraw.filledSquare(.7, .7, .0625);
+		
+		
+		
 	}
 
 }
