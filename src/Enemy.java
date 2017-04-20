f8f9840da06c2201e1bc4c6d4bc230f67baf93f6
student <student@w431-imac01.ccsd.d5>
2017-04-20 07:50:49 -0600

    lots of changes and added choose first item

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,14 +1,19 @@
 
 public abstract class Enemy extends Fighter{
 	private boolean isBoss;
-	public Enemy(int h, int d, boolean b){
+	private String name;
+	public Enemy(int h, int d, boolean b, String n){
 		super(h,d);
 		isBoss = b;
+		name = n;
 	}
 	public boolean isBoss(){
 		return isBoss;
 	}
 	public String toString(){
-		return "Enemy Health =" + this.getHealth();
+		return name + " Health = " + this.getHealth();
+	}
+	public String getName(){
+		return name;
 	}
 }
