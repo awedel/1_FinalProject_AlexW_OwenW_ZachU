d78e9e8eab198c448792ea7b367fdaa28987efac
Zach <Zach@Zach-PC>
2017-04-26 18:13:38 -0600

    Battle Dungeon title created

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,7 @@
 //for testing in console
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Game {
@@ -87,9 +90,38 @@ public class Game {
 	//main
 
 	public static void main(String[] args) {
+		
+		//title
+
+        int width = 300;
+        int height = 30;
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setFont(new Font("Verdana", Font.BOLD, 24));
+
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics.drawString("Battle Dungeon", 10, 20);
+
+        for (int y = 0; y < height; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < width; x++) {
+
+                sb.append(image.getRGB(x, y) == -16777216 ? " " : "0");
+
+            }
+
+            if (sb.toString().trim().isEmpty()) {
+                continue;
+            }
+
+            System.out.println(sb);
+        }
+
 		Game game = new Game();
 		game.playGame();
-
 	}
 
 }
